65b993f0e6aa21ee1a2e1cfedac99bb46e7a8e9f
Mystery00 <mystery0dyl520@gmail.com>
2016-12-15 13:55:35 +0800

    backup;

--- src/Const/Constant.java
+++ src/Const/Constant.java
@@ -7,7 +7,7 @@ public class Constant
 	public final static String TABLENAME_STUDENT = "student";
 	public final static String TABLENAME_SCORE = "score";
 	public final static String TABLENAME_CLASS="class";
-	public final static String TABLE_COLUMNS_USER = "username text,password text";
+	public final static String TABLE_COLUMNS_USER = "username text,password text,manager bool";
 	public final static String TABLE_COLUMNS_STUDENT = "num char(14),name text,sex integer,professional text,college text,birthday text,address text,phone char(12)";
 	public final static String TABLE_COLUMNS_SCORE = "num char(14),code char(10),score int";
 	public final static String TABLE_COLUMNS_CLASS="name text,code text";
